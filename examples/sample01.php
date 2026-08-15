5a125337a2e55f9932ce16616e8b3af8143c4750
ytahara-byte <ythr.mobile@gmail.com>
2026-08-15 18:14:17 +0900

    ファイル構造変更

---
+++ examples/sample01.php
@@ -0,0 +1,17 @@
+<?php
+define( "TMPL_DIR", __DIR__ . '/Templates/');
+require_once(__DIR__ . '/../src/tmpl2.php');
+
+use Tmpl2\Tmpl2;
+
+$tmpl2 = new Tmpl2(TMPL_DIR . 'sample01.html');
+$tmpl2->assign('TITLE', 'sample01');
+$tmpl2->assign('THEME', 'This is a simple theme.');
+$tmpl2->assign('LIST1', 'First list item');
+$tmpl2->assign('LIST2', 'Second list item');
+$tmpl2->assign('LIST3', 'Third list item');
+$tmpl2->assign('COPYRIGHT', 'Copyright (c) 2023 Your Company. All rights reserved.');
+
+echo $tmpl->render();
+
+?>
